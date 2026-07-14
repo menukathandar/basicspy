e4d5b3d2be8ce6cd7996db88332437939651ebd0
Menuka <menuka@Menukas-MacBook-Pro.local>
2026-07-14 14:40:53 +1000

    Added how to find the last and first digit  of a number

--- files/last_first_digit.py
+++ files/last_first_digit.py
@@ -1,1 +1,7 @@
 #5. Display the last digit and first digit of the given number.
+num = 5678
+num1 = num % 10 
+num2 = int(str(num)[::-1])
+first_digit = num2 % 10
+print("The last digit is ", num1)
+print("The first digit is ", first_digit)
