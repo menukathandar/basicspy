d4c003cfc44e343d7b07d21a16404688d514cb02
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-07-20 14:39:50 +1000

    Added how to greet user using function

---
+++ files/greet_using_func.py
@@ -0,0 +1,5 @@
+#Take the user’s name using the input function and greet him.
+def greet(name):
+    print("Hello", name)
+your_name = input("Enter your name: ")
+greet(your_name)
---
+++ files/std_marks.py
@@ -0,0 +1,5 @@
+#7. Take marks of N students from the user and display the Pass percentage. Assume
+#50 is the pass mark.
+num = 5
+no_of_items = len(num)
+print(no_of_items)
