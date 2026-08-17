9e721b297d971667cba4dca9330c6e09bf8e1472
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-08-17 14:31:56 +1000

    Slicingin py

--- files/lists.py
+++ files/lists.py
@@ -5,3 +5,19 @@ my_list = [10,20,30]
 print(my_list) #prints [10, 20, 30]
 print(my_list[1]) # Prints 20 
 print(my_list[-3]) # Prints 10
+my_list.append('new1')
+print(my_list)
+my_list.pop()
+print(my_list)
+
+
+#Slicing in python
+numbers = list(range(5))
+print(numbers)
+print(numbers[2:4]) # Prints 2,3
+print(numbers[2:]) # prints 2,3,4
+print(numbers[:2]) #prints 0,1
+print(numbers[:]) # Gets a slice of whole lists and prints all elements
+print(numbers[:-1]) #prints 0,1,2,3
+numbers[2:4] = [25,35] # Assign new numbers 
+print(numbers) # Prints [0, 1, 25, 35, 4]
