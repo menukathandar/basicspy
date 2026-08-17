146dc99679c27ba4238b45a049714ec6a273e6e0
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-08-17 15:35:20 +1000

    Dictionaries in python

---
+++ files/dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionaries in python stores (key, value) pairs 
+dictionary = {'Sun' : 'Sunny', 'mon' : 'Windy', 'Tue' : 'Cloudy'}
+print(dictionary['Sun']) # Prints sunny
+print('Sunny' in dictionary) # Prints false because "in" checks for key in the dictionary not the value
+print('sun' in dictionary) #FAlse (Case sensitive)
+print('Sun' in dictionary) # True
+dictionary['Wed'] = 'Rainy'
+print(dictionary)
+
+print(dictionary.get('Sun', 'N/A')) # Prints Sunny
+print(dictionary.get('Thurs', 'N/A')) # Prints N/A
+del dictionary['Wed'] # removes elements
+print(dictionary)
