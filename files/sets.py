4153995ee8e94bae450596697b7b807ee67d0f04
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-08-17 17:33:58 +1000

    sets

--- files/sets.py
+++ files/sets.py
@@ -4,3 +4,11 @@ animals = {'cat' , 'dog' , 'monkey'}
 print('cat' in animals)
 animals.add('lion')
 print(animals)
+
+# Since sets are unordered so we cannot make assumptions 
+# about the order in which we visit the elements of the set
+for idx, animal in enumerate(animals):
+    print(idx+1,animal)
+animals.remove('lion')
+print(f'The length of the elements in the given set is {len(animals)}.')
+print(animals)
