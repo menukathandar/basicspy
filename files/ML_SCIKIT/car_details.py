a59af7370844f800a84b8dad27a755821f34051c
Menuka <167495853+menukathandar@users.noreply.github.com>
2026-08-18 16:37:48 +1000

    Removing units and changing datatype of some columns

--- files/ML_SCIKIT/car_details.py
+++ files/ML_SCIKIT/car_details.py
@@ -16,3 +16,41 @@ print(dataset.head())
 dataset.drop(['name'], axis =1, inplace = True) # axis = 0 means dropping the rows and 1 means columns
 print(dataset.head())
 
+
+# 3. Dealing with missing values 
+# i. Identifying if there are any missing values
+print(dataset.isna().sum())
+
+#ii. Dropping the rows which has missing values
+dataset = dataset.dropna()
+print(f'The length of the dataset is {len(dataset)}.')
+
+#4. Dealing with duplicated rows
+#i. Checking if there is any duplicated rows in the dataset
+print(dataset.duplicated().any()) # we use any() here to stop scanning the duplicates the moment it finds true so that it's not much time consuming even for larger datasets
+
+#ii. Dropping all the duplicates values
+dataset = dataset.drop_duplicates()
+print(f'The length of the dataset after dropping duplicates is {len(dataset)}.')
+
+#Checking the datatypes of each column 
+print(dataset.dtypes)
+
+#5. Removing units from column mileage, engine and max_power
+def remove_unit(df, column_name):
+    t = []
+    for i in df[column_name]:
+        number = str(i).split(' ')[0]
+        t.append(number)
+    return t
+dataset['mileage'] = remove_unit(dataset, 'mileage')
+dataset['engine'] = remove_unit(dataset, 'engine')
+dataset['max_power'] = remove_unit(dataset, 'max_power')
+
+#Changing the datatype from object to string
+#dataset['mileage'] = dataset['mileage'].to_string() we can't do this because it's job is to render as one big string not each column
+dataset['mileage'] = pd.to_numeric(dataset['mileage']) # here we are using to_numeric because (eg. "23.40" is still a string just it's unit is removed. so we need to convert it to numeric)
+dataset['engine'] = pd.to_numeric(dataset['engine'])
+dataset['max_power'] = pd.to_numeric(dataset['max_power'])
+print(dataset.head())
+
